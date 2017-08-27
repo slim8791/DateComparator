414d30ad72b49af655719deabcd2c028f44b1cab
Slim HAMMAMI <slim.hammami.ext@easytek.tn>
2017-08-27 11:28:19 +0100

    compare commit

---
+++ ComapreDates/Compare.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComapreDates
+{
+    public class Compare
+    {
+        //this method compare dates only (without time)
+        public static int CompareDatesOnly (DateTime d1, DateTime d2)
+        {
+            int b = 0;
+            if (d1.Year > d2.Year)
+            {
+                b = 1;
+            }
+            else if (d1.Year < d2.Year)
+            {
+                b = -1;
+            }
+            else
+            {
+                if (d1.Month > d2.Month)
+                {
+                    b = 1;
+                }
+                else if(d1.Month < d2.Month)
+                {
+                    b = -1;
+                }
+                else
+                {
+                    if (d1.Day > d2.Day)
+                    {
+                        b = 1;
+                    }
+                    else if (d1.Day < d2.Day)
+                    {
+                        b = -1;
+                    }
+                }
+            }
+            return b;
+        }
+
+        public static int CompareTimeOnly(DateTime d1, DateTime d2)
+        {
+            int b = 0;
+            if (d1.Hour > d2.Hour)
+            {
+                b = 1;
+            }
+            else if(d1.Hour < d2.Hour)
+            {
+                b = -1;
+            }
+            else
+            {
+                if (d1.Minute > d2.Minute)
+                {
+                    b = 1;
+                }
+                else if (d1.Minute<d2.Minute)
+                {
+                    b = -1;
+                }
+                else
+                {
+                    if (d1.Second > d2.Second)
+                    {
+                        b = 1;
+                    }
+                    else if(d1.Second < d2.Second)
+                    {
+                        b = -1;
+                    }
+                    else if(d1.Millisecond > d2.Millisecond)
+                    {
+                        b = 1;
+                    }
+                    else if(d1.Millisecond < d2.Millisecond)
+                    {
+                        b = -1;
+                    }
+
+                }
+            }
+            return b;
+        }
+
+        public static int CompareDateTime(DateTime d1, DateTime d2)
+        {
+            return d1.CompareTo(d2);
+        }
+    }
+}
